835d18e45e47b7537db6c8f6bf00a305506a3933
Subrat Rout <subratnrout@gmail.com>
2021-12-25 11:52:59 -0600

    Added post scaffold

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbo:load", () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})
