ad2e305f4e69cdd4c926c9dbfd8d4924f05ccd93
Subrat Rout <subratnrout@gmail.com>
2022-07-20 20:30:05 -0500

    Add unit test for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'user with a valid email should be valid' do
+    user = User.new(email:"test@subrat.com", password_digest: "password")
+    assert user.valid?
+  end
+
+  test 'user with a invalid email should be valid' do
+    user = User.new(email:"subrat.subrat.com", password_digest: "password")
+    assert_not user.valid?
+  end
+
+  test 'user with taken email should be invalid' do 
+    other_user = users(:one) 
+    user = User.new(email: other_user.email, password_digest: 'test') 
+    assert_not user.valid? 
+  end 
 end
