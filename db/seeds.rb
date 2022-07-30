4c2b81d1f6007f8f5aa9e1676ad6116f987da1d2
Subrat Rout <subratnrout@gmail.com>
2022-07-30 11:59:11 -0500

    Delete a product, db seed and test

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,19 @@
     start_date: rand(1..1000).days.ago.to_date,
   )
 end
+
+Product.delete_all
+User.delete_all
+3.times do
+    user = User.create! email: Faker::Internet.email, password:'locadex1234'
+    puts "Created a new user: #{user.email}"
+    2.times do
+        product = Product.create!(
+        title: Faker::Commerce.product_name,
+        price: rand(1.0..100.0),
+        published: true,
+        user_id: user.id
+        )
+        puts "Created a brand new product: #{product.title}"    
+    end
+end
